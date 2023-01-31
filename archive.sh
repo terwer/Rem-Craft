008c8c2490a50fba4f9a9c6bb0f3413f5a02b2e4
terwer <youweics@163.com>
2023-01-31 11:34:40 +0800

    feat: 新增打包脚本

--- archive.sh
+++ archive.sh
@@ -2,5 +2,6 @@
 
 mkdir -p build
 git archive -o build/Rem-Craft-Plus.zip HEAD
+cp -r build/Rem-Craft-Plus.zip ../SiYuanWorkspace/public/conf/appearance/themes
 
-unzip build/Rem-Craft-Plus.zip
+unzip ../SiYuanWorkspace/public/conf/appearance/themes/Rem-Craft-Plus.zip -d ../SiYuanWorkspace/public/conf/appearance/themes/Rem-Craft-Plus
